f63f606055cbd35408fc9a1b4c8c8bfd2e990ead
Andrei Lineishchikov <andrei.lineishchikov@semrush.com>
2024-11-29 12:35:35 +0100

    add com.local.KeyRemapping.plist

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,3 +1,4 @@
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 defaults -currentHost write -g AppleFontSmoothing -int 0
 defaults write -g ApplePressAndHoldEnabled -bool false
+copy com.local.KeyRemapping.plist ~/Library/LaunchAgents/
